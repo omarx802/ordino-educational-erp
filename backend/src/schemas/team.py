55619446f5545341f02cc8eb95ddee88cfebae47
Omar Souid <omarsouid21@gmail.com>
2025-01-06 01:18:17 +0100

    ProtectingRoutes&Refact1.2

--- backend/src/schemas/team.py
+++ backend/src/schemas/team.py
@@ -6,4 +6,4 @@ class TeamBase(BaseModel):
     total_revenue: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
--- frontend/src/app/inside/dashboard/page.tsx
+++ frontend/src/app/inside/dashboard/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
 import {
@@ -7,7 +7,6 @@ import {
     CardHeader,
     CardTitle,
   } from "@/src/components/ui/card"
-
 
 
 const data = [
